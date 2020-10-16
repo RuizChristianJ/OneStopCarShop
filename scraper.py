8912fde1252260d46687b0fdcc6db760fd73bdf6
RuizChristianJ <38219951+RuizChristianJ@users.noreply.github.com>
2020-10-15 23:13:52 -0400

    switch from requests to selenium for js htnl that was needed

--- scraper.py
+++ scraper.py
@@ -3,6 +3,9 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 zip_code = '06066'
 distance = ['10', '25', '50', '75', '100', '150', '200', '500', '50000']
@@ -14,13 +17,19 @@ max_year = 'c27605'
 min_year = 'c24781'
 
 URL = f'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=' \
-      f'{zip_code}&showNegotiable=true&sortDir=ASC&sourceContext=carGurusHomePageModel&distance={distance[-1]}&' \
+      f'{zip_code}&showNegotiable=true&sortDir=ASC&sourceContext=carGurusHomePageModel&distance={distance[8]}&' \
       f'entitySelectingHelper.selectedEntity2={max_year}&sortType=AGE_IN_DAYS&' \
-      f'entitySelectingHelper.selectedEntity={min_year}{max_mileage[1]}{mileage}{transmission}{transmission_type[1]}'
+      f'entitySelectingHelper.selectedEntity={min_year}{max_mileage[1]}{mileage}{transmission[1]}{transmission_type[1]}'
 
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r"C:\bin\chromedriver_win32\chromedriver.exe")
+driver.get(URL)
+time.sleep(3)
+page = driver.page_source
+driver.quit()
+soup = BeautifulSoup(page, 'html.parser')
 
 script = soup.find_all('script')
 
@@ -31,6 +40,13 @@ while listings == '':
         listings = str(script[counter])
     counter += 1
 
-listings = listings.split(' window.__PREFLIGHT__')[1].strip()[2:-1]
-listings = json.loads(listings)
-listings = listings['listings']
+# listings = listings.split(' window.__PREFLIGHT__')[1].strip()[2:-1]
+# listings = json.loads(listings)
+# listings = listings['listings']
+
+span = soup.find_all('span')
+
+for val in span:
+    if 'page-navigation-number-of-listings' in str(val):
+        print(val)
+        break
