00d911d5cf94116199a7e8e07b1bcaa104d27ff0
RuizChristianJ <38219951+RuizChristianJ@users.noreply.github.com>
2020-10-16 22:50:41 -0400

    working on load and saving data

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import math
 
 zip_code = '06066'
 distance = ['10', '25', '50', '75', '100', '150', '200', '500', '50000']
@@ -21,32 +22,78 @@ URL = f'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInve
       f'entitySelectingHelper.selectedEntity2={max_year}&sortType=AGE_IN_DAYS&' \
       f'entitySelectingHelper.selectedEntity={min_year}{max_mileage[1]}{mileage}{transmission[1]}{transmission_type[1]}'
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(chrome_options=options, executable_path=r"C:\bin\chromedriver_win32\chromedriver.exe")
-driver.get(URL)
-time.sleep(3)
-page = driver.page_source
-driver.quit()
-soup = BeautifulSoup(page, 'html.parser')
+def get_listings(soup):
+    scripts = soup.find_all('script')
+    listings = ''
+    for script in scripts:
+        if 'window.__PREFLIGHT__ = ' in str(script):
+            listings = str(script)
+            break
+    listings = listings.split(' window.__PREFLIGHT__')[1].strip()[2:-1]
+    listings = json.loads(listings)
+    listings = listings['listings']
+    return listings
 
-script = soup.find_all('script')
+def get_car_data(zip: str, min_year_id: str, max_year_id: str, distance: int, transmission: str=None,
+                 max_mileage: str=None):
 
-listings = ''
-counter = 0
-while listings == '':
-    if 'window.__PREFLIGHT__ = ' in str(script[counter]):
-        listings = str(script[counter])
-    counter += 1
+    page_no = 1
+    transmission_str = ''
+    max_mileage_str = ''
+    if transmission is not None:
+        transmission_str = f'&transmission={transmission}'
+    if max_mileage is not None:
+        max_mileage_str = f'&maxMileage={max_mileage}'
 
-# listings = listings.split(' window.__PREFLIGHT__')[1].strip()[2:-1]
-# listings = json.loads(listings)
-# listings = listings['listings']
+    URL = f'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=' \
+          f'{zip_code}&showNegotiable=true&sortDir=ASC&sourceContext=carGurusHomePageModel' \
+          f'&distance={distance}&entitySelectingHelper.selectedEntity2={max_year_id}&sortType=AGE_IN_DAYS&' \
+          f'entitySelectingHelper.selectedEntity={min_year_id}{max_mileage_str}{transmission_str}#resultsPage={page_no}'
 
-span = soup.find_all('span')
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=options, executable_path=r"C:\bin\chromedriver_win32\chromedriver.exe")
+    driver.get(URL)
+    time.sleep(3)
+    page = driver.page_source
+    driver.quit()
+    soup = BeautifulSoup(page, 'html.parser')
 
-for val in span:
-    if 'page-navigation-number-of-listings' in str(val):
-        print(val)
-        break
+    listings = get_listings(soup)
+
+    span = soup.find_all('span')
+
+    num_listings = 0
+    for val in span:
+        if 'page-navigation-number-of-listings' in str(val):
+            num_listings = int(str(val).split('<strong>')[-1].split('<')[0])
+            break
+
+    num_pages = 0
+    if num_listings > 0:
+        num_pages = math.ceil(num_listings/15.0)
+
+    page_no += 1
+    while page_no <= num_pages:
+        URL = f'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=' \
+              f'{zip_code}&showNegotiable=true&sortDir=ASC&sourceContext=carGurusHomePageModel' \
+              f'&distance={distance}&entitySelectingHelper.selectedEntity2={max_year_id}&sortType=AGE_IN_DAYS&' \
+              f'entitySelectingHelper.selectedEntity={min_year_id}{max_mileage_str}{transmission_str}' \
+              f'#resultsPage={page_no}'
+
+        page = requests.get(URL)
+        soup = BeautifulSoup(page, 'html.parser')
+        scripts = soup.find_all('script')
+        listings = ''
+        for script in scripts:
+            if 'window.__PREFLIGHT__ = ' in str(script):
+                listings = str(script)
+                break
+        listings = listings.split(' window.__PREFLIGHT__')[1].strip()[2:-1]
+        listings = json.loads(listings)
+        listings = listings['listings']
+
+
+
+
