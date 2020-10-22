0b1f9c5589420c2797630960aeb5edf5168a62b9
RuizChristianJ <38219951+RuizChristianJ@users.noreply.github.com>
2020-10-22 14:36:00 -0400

    updates on scraper

--- scraper.py
+++ scraper.py
@@ -22,6 +22,70 @@ URL = f'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInve
       f'entitySelectingHelper.selectedEntity2={max_year}&sortType=AGE_IN_DAYS&' \
       f'entitySelectingHelper.selectedEntity={min_year}{max_mileage[1]}{mileage}{transmission[1]}{transmission_type[1]}'
 
+
+def get_make_id():
+    URL = f'https://www.cargurus.com'
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=options, executable_path=r"C:\bin\chromedriver_win32\chromedriver.exe")
+    driver.get(URL)
+    time.sleep(3)
+    page = driver.page_source
+    driver.quit()
+    soup = BeautifulSoup(page, 'html.parser')
+    optgroup = soup.find_all('optgroup')
+
+    all_makes = ''
+    for group in optgroup:
+        if 'All Makes' in str(group):
+            all_makes = str(group)
+            break
+    all_makes = all_makes.split('<option> </option')
+    return all_makes
+
+
+def get_model_id(makes_dict):
+    make_model_dict = {}
+    makes = list(makes_dict.keys())
+
+    for make in makes:
+        make_id = makes_dict[make]
+
+        URL = f'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?' \
+              f'sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity={make_id}&zip=06066'
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(chrome_options=options, executable_path=r"C:\bin\chromedriver_win32\chromedriver.exe")
+        driver.get(URL)
+        time.sleep(3)
+        page = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(page, 'html.parser')
+        optgroup = soup.find_all('optgroup')
+
+        all_models = ''
+        for group in optgroup:
+            if 'Popular Models' in str(group):
+                all_models = str(group)
+                break
+
+        models = all_models.split('option value=')
+        for i in range(0, len(models)):
+            models[i] = models[i].replace('"', '')
+            models[i] = models[i].replace('>', ' ')
+            models[i] = models[i].split('<')[0]
+
+        models = models[1:]
+        models_dict = {}
+        for model in models:
+            ssplit = model.split(' ')
+            models_dict[ssplit[1]] = ssplit[0]
+
+        make_model_dict[make] = models_dict
+    return make_model_dict
+
 def get_listings(soup):
     scripts = soup.find_all('script')
     listings = ''
@@ -93,7 +157,4 @@ def get_car_data(zip: str, min_year_id: str, max_year_id: str, distance: int, tr
         listings = listings.split(' window.__PREFLIGHT__')[1].strip()[2:-1]
         listings = json.loads(listings)
         listings = listings['listings']
-
-
-
-
+    return None
