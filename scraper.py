01772e3281f87a1819cf58a9fbb4b83963824f1c
RuizChristianJ <38219951+RuizChristianJ@users.noreply.github.com>
2020-11-02 22:01:00 -0500

    added helper functions for reused code. Need to generalize more

--- scraper.py
+++ scraper.py
@@ -7,6 +7,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import math
+from enum import Enum
+import random
+
 
 zip_code = '06066'
 distance = ['10', '25', '50', '75', '100', '150', '200', '500', '50000']
@@ -27,101 +30,95 @@ query_URL = f'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterVi
       f'entitySelectingHelper.selectedEntity={min_year}{max_mileage[1]}{mileage}{transmission[1]}{transmission_type[1]}'
 
 
-def get_make_id(popular: bool = True):
-    # Get all makes or just the "popular" ones (as defined by CarGurus).
-    if popular:
-        make_selection = 'Popular Makes'
-    else:
-        make_selection = 'All Makes'
+class SelectionEnum(Enum):
+    allMakes = 'All Makes'
+    popMakes = 'Popular Makes'
 
-    make_url = f'https://www.cargurus.com'
+    Models = ['Popular Models', 'Other Models']
+
+    Listing = 'window.__PREFLIGHT__ = '
+
+    pageNo = 'page-navigation-number-of-listings'
+
+
+#TODO Add option for two grouping types. Comes in handy for listing getter.
+def get_soup_group(grouping: str, url: str):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(chrome_options=options, executable_path=r"C:\bin\chromedriver_win32\chromedriver.exe")
-    driver.get(make_url)
-    time.sleep(3)
+    driver.get(url)
+    sleep_timer = random.randint(3, 15)
+    time.sleep(sleep_timer)
     page = driver.page_source
     driver.quit()
     soup = BeautifulSoup(page, 'html.parser')
-    optgroup = soup.find_all('optgroup')
+    groups = soup.find_all(grouping)
 
-    makes = ''
-    for group in optgroup:
-        if make_selection in str(group):
-            makes = str(group)
-            break
-    makes = makes.split('option value=')
-    for i in range(0, len(makes)):
-        makes[i] = makes[i].replace('"', '')
-        makes[i] = makes[i].replace('>', ' ')
-        makes[i] = makes[i].split('<')[0]
-    makes = makes[1:]
-    makes_dict = {}
-    for make in makes:
-        ssplit = make.split(' ')
-        makes_dict[ssplit[1]] = ssplit[0]
+    if len(groups) == 0:
+        raise KeyError
 
-    return makes_dict
+    return soup
 
 
+#TODO Add option for two grouping types. Comes in handy for listing getter.
+def get_selection(url: str, selection_type: SelectionEnum):
+    optgroup = get_soup_group('optgroup', url)
 
-def get_model_id(popular: bool = True, make_id: str = None):
+    selection = []
+    if selection_type == SelectionEnum.Models:
+        for i in range(0, len(selection_type.value)):
+            for group in optgroup:
+                if selection_type.value[i] in str(group):
+                    selection.append(str(group))
+                    break
+        return selection
+    else:
+        for group in optgroup:
+            if selection_type.value in str(group):
+                selection.append(str(group))
+                break
+        return selection
+
+
+def clean_selection(selection: str):
+    selection_cleaned = selection.split('option value=')
+    for i in range(0, len(selection_cleaned)):
+        selection_cleaned[i] = selection_cleaned[i].replace('"', '')
+        selection_cleaned[i] = selection_cleaned[i].replace('>', ' ')
+        selection_cleaned[i] = selection_cleaned[i].split('<')[0]
+    selection_cleaned = selection_cleaned[1:]
+    selection_dict = {}
+    for value in selection_cleaned:
+        ssplit = value.split(' ')
+        selection_dict[ssplit[1]] = ssplit[0]
+    return selection_dict
+
+
+def get_make_id(selection_type: SelectionEnum):
+    # Get all makes or just the "popular" ones (as defined by CarGurus).
+    if selection_type not in [SelectionEnum.allMakes, SelectionEnum.popMakes]:
+        raise KeyError
+
+    make_url = f'https://www.cargurus.com'
+
+    makes = get_selection(make_url, selection_type)[0]
+    makes = clean_selection(makes)
+
+    return makes
+
+
+def get_model_id(make_id: str = None):
 
     model_url = f'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?' \
           f'sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity={make_id}'
 
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(chrome_options=options, executable_path=r"C:\bin\chromedriver_win32\chromedriver.exe")
-    driver.get(model_url)
-    time.sleep(3)
-    page = driver.page_source
-    driver.quit()
-    soup = BeautifulSoup(page, 'html.parser')
-    optgroup = soup.find_all('optgroup')
+    models = get_selection(model_url, SelectionEnum.Models)
+    models_dict = clean_selection(models[0])
+    models_dict.update(clean_selection(models[1]))
 
-    popular_models = ''
-    other_models = ''
-    if popular:
-        for group in optgroup:
-            if 'Popular Models' in str(group):
-                popular_models = str(group)
-                break
-    else:
-        for group in optgroup:
-            if 'Popular Models' in str(group):
-                popular_models = str(group)
-                continue
-            if 'Other Models' in str(group):
-                other_models = str(group)
-                continue
+    return models_dict
 
-    popular_models = popular_models.split('option value=')
-    for i in range(0, len(popular_models)):
-        popular_models[i] = popular_models[i].replace('"', '')
-        popular_models[i] = popular_models[i].replace('>', ' ')
-        popular_models[i] = popular_models[i].split('<')[0]
-    popular_models = popular_models[1:]
-    models = popular_models
-
-    if other_models != '':
-        other_models = other_models.split('option value=')
-        for i in range(0, len(popular_models)):
-            other_models[i] = other_models[i].replace('"', '')
-            other_models[i] = other_models[i].replace('>', ' ')
-            other_models[i] = other_models[i].split('<')[0]
-        other_models = other_models[1:]
-        models = models + other_models
-
-    make_model_dict = {}
-    for model in models:
-        ssplit = model.split(' ')
-        make_model_dict[ssplit[1]] = ssplit[0]
-
-    make_model_dict[make_id] = make_model_dict
-    return make_model_dict
 
 def get_listings(soup):
     scripts = soup.find_all('script')
@@ -134,6 +131,7 @@ def get_listings(soup):
     listings = json.loads(listings)
     listings = listings['listings']
     return listings
+
 
 def get_car_data(zip: str, min_year_id: str, max_year_id: str, distance: int, transmission: str=None,
                  max_mileage: str=None):
@@ -195,3 +193,7 @@ def get_car_data(zip: str, min_year_id: str, max_year_id: str, distance: int, tr
         listings = json.loads(listings)
         listings = listings['listings']
     return None
+
+
+test = get_model_id('m42')
+print(json.dumps(test, indent=3))
